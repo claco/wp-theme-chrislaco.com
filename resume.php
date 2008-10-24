abbc9769c36f33322c458f805c1089c5fc6693b1
Christopher H. Laco <claco@chrislaco.com>
2008-10-23 21:35:13 -0400

    Updated styles for resume

--- resume.php
+++ resume.php
@@ -4,5 +4,18 @@ Template Name: Resume
 */
 ?>
 <?php get_header();?>
-
+<?php if(have_posts()):?>
+	<?php while(have_posts()):the_post();?>
+		<div class="post">
+			<div class="content">
+				<?php the_content();?>
+			</div>
+			<?php if (get_the_tags()):?>
+			<div class="tags">
+				Tags: <?php the_tags('<div class="tag">','</div><div class="tag">','</div>');?>
+			</div>
+			<?php endif;?>
+		</div>
+	<?php endwhile;?>
+<?php endif;?>
 <?php get_footer();?>
