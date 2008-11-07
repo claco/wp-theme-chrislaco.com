4a2d1d57b9dfe57bfadba0f030fc1d911c3a2211
Christopher H. Laco <claco@chrislaco.com>
2008-11-06 20:57:57 -0500

    Fixed title tag

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-		<title><?php bloginfo(’name’);?></title>
+		<title><?php bloginfo(’name’);?><?php wp_title(); ?></title>
 		<link rel="stylesheet" href="<?php echo dirname(get_bloginfo('stylesheet_url'));?>/reset.css" type="text/css" media="all" charset="utf-8" />
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" media="screen" charset="utf-8" />
 		<?php wp_head();?>
