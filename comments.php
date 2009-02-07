d0497b247f7c14990057a391b4a7afc124a89ed0
Christopher H. Laco <claco@chrislaco.com>
2009-02-07 12:27:34 -0500

    Added code/style for comment display on entry pages

--- comments.php
+++ comments.php
@@ -1,11 +1,23 @@
 <?php if ( have_comments() ) : ?>
+<a name="comments"></a>
 <div class="comments">
     <h3 class="title">Comments</h3>
     <?php while(have_comments()):the_comment();?>
         <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
+        <?php include('comment.php');?>
     <?php endwhile;?>
 </div>
 <?php endif; ?>
-
-<hr />
-<?php wp_list_comments('style=div'); ?>
--- js/site.js
+++ js/site.js
@@ -17,4 +17,16 @@ $(document).ready(function(){
     $('#header .menu, #footer .copyright, #content .extras').each(function(){
         $(this).find('li:not(:last)').after('<li class="separator">&middot;</li>');
     })
+
+    // comments
+    $('.comment').each(function(i) {
+        i = i+1;
+        $(this).prepend('<div class="number">'+i+'</div>');
+        
+        if (i % 2 == 0) {
+            $(this).addClass('even');
+        } else {
+            $(this).addClass('odd');
+        }
+    })    
 });
